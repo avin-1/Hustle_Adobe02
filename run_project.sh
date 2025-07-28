4bfa3f8311a9c85436afc838c8b6f7edf5307419
avin-1 <149309916+avin-1@users.noreply.github.com>
2025-07-28 19:23:38 +0530

    Update run_project.sh

--- run_project.sh
+++ run_project.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+echo "Installing requirements..."
+pip3 install -r requirements.txt
 
 echo "Running download_model.py..."
 python3 download_model.py
-
-echo "Installing requirements..."
-pip3 install -r requirements.txt
 
 echo "Running main.py..."
 python3 src/main.py
